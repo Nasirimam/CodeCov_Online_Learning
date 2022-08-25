26648eabbc13c9813b56e3d60b648a70266f8912
Nasirimam <soulpanther4@gmail.com>
2022-08-25 20:07:31 +0530

    added day-3 work

---
+++ HomePage/home.js
@@ -0,0 +1,10 @@
+import { mainNavbar , mainNavbarCSS } from "./navbar.js";
+
+let temp = mainNavbar()
+
+let style = document.getElementById ('style');
+
+let container = document.getElementById ('container')
+container.innerHTML = temp
+
+style.innerText = mainNavbarCSS()
