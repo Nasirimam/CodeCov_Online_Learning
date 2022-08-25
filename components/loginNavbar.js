1a921c9e6bdc36b722dba6c0003056b7e7fd9999
pranshubisht9 <pranshubisht9@gmail.com>
2022-08-25 15:22:44 +0530

    add day-3 project work

---
+++ components/loginNavbar.js
@@ -0,0 +1,25 @@
+
+// Made By Pranshu Singh Bisht (fw20_0423)
+
+function loginNavbar(){
+    return `
+    <nav id="loginNav">
+    <div id="leftNav">
+        <img src="images/login_nav_logo.png" alt="logo">
+
+        <a href="">Docs</a>
+        <a href="">Support</a>
+        <a href="">Blog</a>
+    </div>
+    <div id="rightNav">
+        <div>
+            New to Codecov? &nbsp&nbsp
+        </div>
+        <a href="">
+            Learn more
+        </a>
+    </div>
+</nav>
+    `
+}
+export default loginNavbar
