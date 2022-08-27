bf52b4c2310eaa6042fa1bd8f5538b97e4ac1b2b
Nasirimam <soulpanther4@gmail.com>
2022-08-27 18:36:06 +0530

    added day-5 work

--- HomePage/home.js
+++ HomePage/home.js
@@ -8,3 +8,21 @@ let container = document.getElementById ('container')
 container.innerHTML = temp
 
 style.innerText = mainNavbarCSS()
+
+
+import { footer } from "./footer.js";
+
+let temp2 = footer()
+
+let footer_div = document.getElementById('footer_div')
+
+footer_div.innerHTML = temp2
+
+
+import { stickyNavbar } from "./stickyNavbar.js";
+
+let temp3 = stickyNavbar()
+
+let sticky_Nav = document.getElementById('sticky_Nav')
+
+sticky_Nav.innerHTML = temp3
